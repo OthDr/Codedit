774bbef9a43a31b2dfad63c9767b098e8604103d
abdelhak002 <darbeida.abdelhak@gmail.com>
2022-09-13 23:22:20 +0100

    will be removed

--- src/components/main/Main.jsx
+++ src/components/main/Main.jsx
@@ -1,17 +1,12 @@
-import React, { useState } from 'react';
 import TopBar from './inc/TopBar';
 import BottomBar from './inc/BottomBar';
 import SideBar from './inc/SideBar';
 
-const Main = (props) => {
-  const [content, useContent] = useState('');
+const Main = () => {
   return (
     <main className="bg-[#011627] text-white flex flex-col justify-between h-screen">
       <TopBar />
       <SideBar />
-      {/* <button onClick={readFile('Welcome.jsx')}>Click</button> */}
-      {/* <h1>{content}</h1> */}
-      <section className="flex-auto">{props.children}</section>
       <BottomBar />
     </main>
   );
