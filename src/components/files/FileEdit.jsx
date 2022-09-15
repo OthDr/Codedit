6afa9f5c16608fa93b0c3f8899ee1e1a7c4bba5d
abdelhak002 <darbeida.abdelhak@gmail.com>
2022-09-15 23:26:50 +0100

    feat(component): create FileEdit get file name, content, extension and folder path from sideBar send file name, extension, folder path to top bar

--- src/components/files/FileEdit.jsx
+++ src/components/files/FileEdit.jsx
@@ -1,8 +1,47 @@
 import React from 'react';
+import { useState } from 'react';
+import BottomBar from '../main/inc/BottomBar';
+import SideBar from '../main/inc/SideBar';
+import TopBar from '../main/inc/TopBar';
 import Main from '../main/Main';
 
 const FileEdit = () => {
-  return <Main>FileEdit</Main>;
+  const [data, setData] = useState({});
+
+  const childToParent = (fileName, fileExtension, fileContent, folderPath) => {
+    setData({
+      fileName: fileName,
+      fileExtension: fileExtension,
+      fileContent: fileContent,
+      folderPath: folderPath,
+    });
+  };
+  // childToParent();
+  const FileToBar = () => {};
+  console.log(data);
+  return (
+    <div className="bg-[#011627] text-white flex flex-col justify-between h-screen">
+      <TopBar
+        fileName={data.fileName}
+        fileExtension={data.fileExtension}
+        folderPath={data.folderPath}
+      />
+      <SideBar childToParent={childToParent} />
+
+      {data.fileContent ? (
+        <div className="flex-auto mr-32 pr-4">
+          <textarea
+            className="text-sm block w-full h-full  bg-transparent placeholder-gray-300 text-white border-1 ring-transparent focus:ring-0 focus:border-sky-500"
+            value={data.fileContent}
+            spellCheck={false}
+          />
+        </div>
+      ) : (
+        ''
+      )}
+      <BottomBar />
+    </div>
+  );
 };
 
 export default FileEdit;
