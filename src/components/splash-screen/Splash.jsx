a007e18cba97a8deeec678ba28aa6a87a6bc8d05
abdelhak002 <darbeida.abdelhak@gmail.com>
2022-10-15 22:59:22 +0100

    feat(component): add link to welcome component

--- src/components/splash-screen/Splash.jsx
+++ src/components/splash-screen/Splash.jsx
@@ -1,17 +1,23 @@
-import React from 'react'
-import { DiCode } from "react-icons/di";
-import { TiArrowRightThick } from "react-icons/ti";
+import React from 'react';
+import { DiCode } from 'react-icons/di';
+import { TiArrowRightThick } from 'react-icons/ti';
+import { Link } from 'react-router-dom';
 
 const Splash = () => {
-    return (
-        <div className='w-full h-screen p-[10%] bg-slate-900'>
-            <div className="mb-[12%] w-fit mx-auto flex items-center ">
-                <h1 className='w-fit text-indigo-400 text-4xl font-bold '>Codeedit</h1>
-                <DiCode className='text-indigo-500' size={65} />
-            </div>
-            <button className='w-fit mx-auto flex items-center text-indigo-200 text-2xl font-bold animate-bounce'>Start <TiArrowRightThick className='text-indigo-200 mx-2' size={30} /></button>
-        </div>
-    )
-}
+  return (
+    <div className="w-full h-screen p-[10%] bg-[#011627]">
+      <div className="mb-[12%] w-fit mx-auto flex items-center ">
+        <h1 className="w-fit text-sky-500 text-4xl font-bold ">Codeedit</h1>
+        <DiCode className="text-sky-600" size={65} />
+      </div>
+      <Link
+        to="/welcome"
+        className="w-fit mx-auto flex items-center text-sky-300 text-2xl font-bold animate-bounce"
+      >
+        Start <TiArrowRightThick className="text-sky-300 mx-2" size={30} />
+      </Link>
+    </div>
+  );
+};
 
-export default Splash
+export default Splash;
