cd17f6c4dcf66d11eef445881b93f7e649ee998b
abdelhak002 <darbeida.abdelhak@gmail.com>
2022-10-15 14:16:04 +0100

    feat(component): add routes to App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,18 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import Main from './components/main/Main'
-import Splash from './components/splash-screen/Splash'
+import React from 'react';
+import { Routes, Route } from 'react-router-dom';
+import Main from './components/main/Main';
+import Welcome from './components/main/Welcome';
+import FileEdit from './components/files/FileEdit';
+import Splash from './components/splash-screen/Splash';
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <div className="App">
-      <Splash />
-      <Main />
-    </div>
-  )
+    <Routes>
+      <Route index element={<Splash />} />
+      <Route path="welcome" element={<Welcome />} />
+      <Route path="file" element={<FileEdit />} />
+    </Routes>
+  );
 }
 
-export default App
+export default App;
