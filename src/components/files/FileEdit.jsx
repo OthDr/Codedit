c4abe4d6b310cc7f55c53a6391d6004300892049
unknown <otmax.vft@gmail.com>
2022-10-23 10:40:07 +0100

    minor changes

--- src/components/files/FileEdit.jsx
+++ src/components/files/FileEdit.jsx
@@ -22,19 +22,19 @@ const FileEdit = () => {
   return <Main>
     <div className='p-2 flex w-full h-full  bg-black/30'>
       <div className='flex flex-col'>
-        <div className='px-4 text-right  font-semibold text-gray-500'>1</div>
-        <div className='px-4 text-right  font-semibold text-gray-500'>2</div>
-        <div className='px-4 text-right  font-semibold text-gray-500'>3</div>
-        <div className='px-4 text-right  font-semibold text-gray-500'>4</div>
-        <div className='px-4 text-right  font-semibold text-gray-500'>5</div>
-        <div className='px-4 text-right  font-semibold text-gray-500'>6</div>
-        <div className='px-4 text-right  font-semibold text-gray-500'>7</div>
-        <div className='px-4 text-right  font-semibold text-gray-500'>8</div>
-        <div className='px-4 text-right  font-semibold text-gray-500'>9</div>
-        <div className='px-4 text-right  font-semibold text-gray-500'>10</div>
-        <div className='px-4 text-right  font-semibold text-gray-500'>11</div>
-        <div className='px-4 text-right  font-semibold text-gray-500'>12</div>
-        <div className='px-4 text-right  font-semibold text-gray-500'>13</div>
+        <div className='px-4 text-right  font-semibold text-gray-500 cursor-pointer'>1</div>
+        <div className='px-4 text-right  font-semibold text-gray-500 cursor-pointer'>2</div>
+        <div className='px-4 text-right  font-semibold text-gray-500 cursor-pointer'>3</div>
+        <div className='px-4 text-right  font-semibold text-gray-500 cursor-pointer'>4</div>
+        <div className='px-4 text-right  font-semibold text-gray-500 cursor-pointer'>5</div>
+        <div className='px-4 text-right  font-semibold text-gray-500 cursor-pointer'>6</div>
+        <div className='px-4 text-right  font-semibold text-gray-500 cursor-pointer'>7</div>
+        <div className='px-4 text-right  font-semibold text-gray-500 cursor-pointer'>8</div>
+        <div className='px-4 text-right  font-semibold text-gray-500 cursor-pointer'>9</div>
+        <div className='px-4 text-right  font-semibold text-gray-500 cursor-pointer'>10</div>
+        <div className='px-4 text-right  font-semibold text-gray-500 cursor-pointer'>11</div>
+        <div className='px-4 text-right  font-semibold text-gray-500 cursor-pointer'>12</div>
+        <div className='px-4 text-right  font-semibold text-gray-500 cursor-pointer'>13</div>
       </div>
 
       <textarea onChange={handleCodeChange} className='px-2 w-full h-full bg-transparent' name="codeEdit" id="codeEdit" cols="30" rows="10">var x = 10 - 2 ; </textarea>
